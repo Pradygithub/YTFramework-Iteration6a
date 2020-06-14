ec94dc3ecbbf9b325dc857e0bf00f0dcf120a0ad
lenovo <lenovo@DESKTOP-IT1KP8Q>
2020-06-15 04:27:40 +0530

    Jenkins GitHub Integration

---
+++ JenkinsGitHubIntegration/src/org/testing/testscripts/JenkinsGitHub.java
@@ -0,0 +1,15 @@
+package org.testing.testscripts;
+
+import org.testng.annotations.Test;
+
+public class JenkinsGitHub 
+{
+
+	@Test
+	public void display()
+	{
+		System.out.println("This is Jenkins Git Hub Integration test");
+	
+			
+			}
+}
